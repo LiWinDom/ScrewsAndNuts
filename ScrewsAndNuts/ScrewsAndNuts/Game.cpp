973830856dbe594a389896c7eb608b0a52f2f8be
LiWinDom <wadim.gorbachev@mail.ru>
2022-11-09 15:34:08 +0300

    One more undo fix

--- ScrewsAndNuts/ScrewsAndNuts/Game.cpp
+++ ScrewsAndNuts/ScrewsAndNuts/Game.cpp
@@ -38,16 +38,19 @@ bool Game::move(const uint8_t& x, const uint8_t& y) {
 }
 
 void Game::undo(const uint8_t& movesNum) {
+	if (_winner != nullptr) {
+		if (_curPlayer == _player1) {
+			_curPlayer = _player2;
+		}
+		else {
+			_curPlayer = _player1;
+		}
+	}
+
 	_curMove -= std::min(_curMove, movesNum);
 	_winner = nullptr;
 	_winCombination = std::vector<std::pair<uint8_t, uint8_t>>(0, std::pair<uint8_t, uint8_t>(0, 0));
 
-	if (_curPlayer == _player1) {
-		_curPlayer = _player2;
-	}
-	else {
-		_curPlayer = _player1;
-	}
 	return;
 }
 
