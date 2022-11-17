c1bcb96ba86bbbad4ab13d7d89723aff7d0c1d2e
BuildTools <wadim.gorbachev@mail.ru>
2022-11-17 22:03:23 +0300

    Some changes in ImposibleBot

--- ScrewsAndNuts/ScrewsAndNuts/Bots/ImpossibleBot.cpp
+++ ScrewsAndNuts/ScrewsAndNuts/Bots/ImpossibleBot.cpp
@@ -3,10 +3,13 @@
 #include "../Game.h"
 
 BotThink ImpossibleBot::think(Game* game) const {
-	BotThink bestMove(std::pair<uint8_t, uint8_t>(0, 0), std::pair<uint16_t, uint16_t>(0, 32000));
-
 	ImpossibleBot enemy(game->getWaitingPlayer()->getNumber());
 	enemy.setThinkingLimit(_thinkingLimit - 1);
+
+	BotThink bestMove(std::pair<uint8_t, uint8_t>(game->getPossibleMoves()[0].first, game->getPossibleMoves()[0].second),
+		std::pair<uint16_t, uint16_t>(0, 32000));
+
+	std::vector<std::pair<uint8_t, uint8_t>> movesNum = game->getPossibleMoves();
 
 	for (uint16_t i = 0; i < game->getPossibleMoves().size(); ++i) {
 		uint8_t x = game->getPossibleMoves()[i].first, y = game->getPossibleMoves()[i].second;
@@ -20,11 +23,16 @@ BotThink ImpossibleBot::think(Game* game) const {
 		}
 		game->undo(1);
 
-		if (curMove.getBestScore().first >= 32000) {
+		if (curMove.getBestScore().first >= 16000 && curMove.getBestScore().second < 32000) {
 			bestMove = curMove;
-			break;
+			if (curMove.getMoves().size() < 2) {
+				break;
+			}
+			continue;
 		}
-		if (curMove.getBestScore().second >= 32000) continue;
+		if (curMove.getBestScore().second >= 16000) {
+			continue;
+		}
 
 		if (curMove.getBestScore().first - curMove.getBestScore().second >= bestMove.getBestScore().first - bestMove.getBestScore().second) {
 			if (curMove.getBestScore().first - curMove.getBestScore().second > bestMove.getBestScore().first - bestMove.getBestScore().second) {
